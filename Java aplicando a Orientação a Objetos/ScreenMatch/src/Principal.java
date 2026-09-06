d2f713557c2baf0d8538aea9c7af7c91026732cb
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2026-09-06 14:16:43 -0300

    Adiciona classe filme a outro pacote

--- Java aplicando a Orientação a Objetos/ScreenMatch/src/Principal.java
+++ Java aplicando a Orientação a Objetos/ScreenMatch/src/Principal.java
@@ -1,3 +1,5 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -9,11 +11,7 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
-        // meuFilme.somaDasAvaliacoes = 10;
-        // meuFilme.totalDeAvaliacoes = 1;
-        //System.out.println(meuFilme.pegaMedia());
     }
 }
