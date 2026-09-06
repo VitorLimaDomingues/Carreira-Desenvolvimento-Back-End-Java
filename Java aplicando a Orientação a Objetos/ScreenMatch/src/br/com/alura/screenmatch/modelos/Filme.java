1f0a390a32668784c60f298526bd7c6fc2a5d6e3
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2026-09-06 14:28:29 -0300

    Criando métodos que modificam valores

--- Java aplicando a Orientação a Objetos/ScreenMatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ Java aplicando a Orientação a Objetos/ScreenMatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,16 +1,53 @@
 package br.com.alura.screenmatch.modelos;
 
 public class Filme {
-    public String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
+    // Orientação a Objetos
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
-    int duracaoEmMinutos;
+    private int duracaoEmMinutos;
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
 
     public int getTotalDeAvaliacoes() {
         return totalDeAvaliacoes;
     }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    // Métodos
 
     public void exibeFichaTecnica() {
         System.out.println("Nome do filme: " + nome);
